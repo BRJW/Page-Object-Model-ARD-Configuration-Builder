ba3c4b8e983741c6e96a440fd8be000c8d9e12bc
William Sault <williamrsault@gmail.com>
2018-03-07 10:14:15 +0000

    Update ConfigAction.java

--- src/com/company/ConfigAction.java
+++ src/com/company/ConfigAction.java
@@ -11,11 +11,12 @@ public class ConfigAction {
     ConfigAction(String Name){
         this.Name = Name;
         Parameters = new ArrayList<ParsedMethodParameter>();
-        CodeSnippet = ""; //in the worst case we have an empty code snippet. - should we have a more explicit failure?
+        CodeSnippet = ""; //in the worst case we have an empty code snippet. - should we have a more explicit failure? Nothing wrong with this - could throw methods that didn't parse correctly to a failure list 
     }
 
     void AddParameter(ParsedMethodParameter Parameter){
         Parameters.add(Parameter);
     }
     //Getters and setters yes or no?
+    //Definetley use them! make the memembers private and then press cmd + N (on mac) you can generate them automtically
 }
